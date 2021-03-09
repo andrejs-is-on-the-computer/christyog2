a100937416c77118be17d957ad32a47382e931b3
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2021-03-09 11:53:22 +0100

    alert update

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
+var ua = navigator.userAgent.toLowerCase(); 
+if (ua.indexOf('safari') != -1) { 
+  if (ua.indexOf('chrome') > -1) {
+    alert("CHROME") // Chrome
+  } else {
+    alert("SAFARI") // Safari
+  }
+}
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-
-console.log("prev: " + prevScrollpos);
-console.log("cur: " + currentScrollPos);
 
 if (prevScrollpos + 1 > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";

c35e447ea7353dfdbba61eb444259481694178a4
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2021-03-09 12:12:43 +0100

    scrollbar update true/false

--- javascript.js
+++ javascript.js
@@ -1,20 +1,26 @@
+var scrollbar = true;
+
 var ua = navigator.userAgent.toLowerCase(); 
 if (ua.indexOf('safari') != -1) { 
   if (ua.indexOf('chrome') > -1) {
     alert("CHROME") // Chrome
+    // scrollbar = true;
   } else {
     alert("SAFARI") // Safari
+    scrollbar = false;
   }
 }
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-
-if (prevScrollpos + 1 > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-} else {
-    document.getElementById("navbar").style.top = "-90px";
-}
-    prevScrollpos = currentScrollPos;
+if (scrollbar) {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    
+    if (prevScrollpos + 1 > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-90px";
+    }
+        prevScrollpos = currentScrollPos;
+    }
 }
